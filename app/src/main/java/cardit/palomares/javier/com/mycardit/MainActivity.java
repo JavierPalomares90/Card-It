a1370285bd903ace0f990219559e514e01e72950
Javier Palomares <javier.palomares.90@gmail.com>
2015-11-01 19:04:42 -0600

    Added view for home card

--- app/src/main/java/cardit/palomares/javier/com/mycardit/MainActivity.java
+++ app/src/main/java/cardit/palomares/javier/com/mycardit/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -11,8 +13,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.support.v4.widget.DrawerLayout;
+import android.widget.TextView;
 
 import cardit.palomares.javier.com.mycardit.cardit.palaomares.javier.com.mycardit.card.Card;
 
@@ -23,13 +28,19 @@ public class MainActivity extends Activity {
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private CharSequence mTitle;
-
+    private Card myCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myCard = new Card("Javier", "Palomares", BitmapFactory.decodeResource(getResources(),R.drawable.android));
 
+        EditText name = (EditText) findViewById(R.id.name);
+        name.setText(myCard.getFirstName() + " " + myCard.getLastName(), TextView.BufferType.EDITABLE);
+
+        ImageView cardView = (ImageView) findViewById(R.id.imageView);
+        cardView.setImageBitmap(myCard.getImg());
         mNavigationDrawerItemTitles = getResources().getStringArray(R.array.planets);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
