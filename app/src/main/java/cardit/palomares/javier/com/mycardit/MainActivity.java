582f8ea3da2266fbc75b05816033f05bcb3b8d65
Javier Palomares <javier.palomares.90@gmail.com>
2015-11-03 22:19:14 -0600

    Added camera activity to snap card pics. Need to track down NPE in saving card.

--- app/src/main/java/cardit/palomares/javier/com/mycardit/MainActivity.java
+++ app/src/main/java/cardit/palomares/javier/com/mycardit/MainActivity.java
@@ -3,40 +3,62 @@ package cardit.palomares.javier.com.mycardit;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import cardit.palomares.javier.com.mycardit.cardit.palaomares.javier.com.mycardit.card.Card;
 
 public class MainActivity extends Activity {
 
-
-    private String[] mNavigationDrawerItemTitles;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
-    private CharSequence mTitle;
     private Card myCard;
     private Card[] cards;
     private EditText name;
     private ImageView cardView;
+    private Button snapCardButton;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap mImageBitmap;
+    private String mCurrentPhotoPath;
+    private static String TAG = "MyCardIt";
+    private static int THUMBNAIL_WIDTH = 750;
+    private static int THUMBNAIL_HEIGHT = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        snapCardButton = (Button) findViewById(R.id.snap_card_button);
+        snapCardButton.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+                 snapCard();
+             }
+         });
+
         myCard = new Card("Javier", "Palomares", BitmapFactory.decodeResource(getResources(),R.drawable.android));
 
         name = (EditText) findViewById(R.id.name);
@@ -44,7 +66,6 @@ public class MainActivity extends Activity {
 
         cardView = (ImageView) findViewById(R.id.imageView);
         cardView.setImageBitmap(myCard.getImg());
-        mNavigationDrawerItemTitles = getResources().getStringArray(R.array.planets);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
@@ -56,6 +77,55 @@ public class MainActivity extends Activity {
         CardViewAdapter adapter = new CardViewAdapter(this, R.layout.contacts_listview_row,cards);
         mDrawerList.setAdapter(adapter);
         mDrawerList.setOnItemClickListener(new CardClickListener());
+    }
+
+    private void snapCard(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.i(TAG, "IOException");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+            imageFileName,  // prefix
+            ".jpg",         // suffix
+            storageDir      // directory
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                mImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(mCurrentPhotoPath));
+                Bitmap thumbnail = ThumbnailUtils.extractThumbnail(mImageBitmap, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
+                cardView.setImageBitmap(thumbnail);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
