fad60f015434536d9e5c0278d3e6a0ffac6d47b8
Javier Palomares <javier.palomares.90@gmail.com>
2015-11-27 15:05:18 -0600

    Fixed snap card

--- app/src/main/java/cardit/palomares/javier/com/mycardit/MainActivity.java
+++ app/src/main/java/cardit/palomares/javier/com/mycardit/MainActivity.java
@@ -1,6 +1,8 @@
 package cardit.palomares.javier.com.mycardit;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -22,6 +24,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -46,6 +49,7 @@ public class MainActivity extends Activity {
     private static int THUMBNAIL_WIDTH = 750;
     private static int THUMBNAIL_HEIGHT = 500;
     private static String cardsFilePath;
+    private static int ADD_CONTACT_REQUEST = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,29 +88,20 @@ public class MainActivity extends Activity {
         mDrawerList.setAdapter(adapter);
         mDrawerList.setOnItemClickListener(new CardClickListener());
     }
-    private static int ADD_CONTACT_REQUEST = 2;
+
 
     private void addContact(){
+        Log.d(TAG,"In add Contact");
         Intent i = new Intent(MainActivity.this,AddContactActivity.class);
         startActivityForResult(i,ADD_CONTACT_REQUEST);
     }
 
     private void snapCard(){
+        Log.d(TAG,"In Snap Card");
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                Log.i(TAG, "IOException");
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
-            }
+
+            startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
@@ -114,8 +109,9 @@ public class MainActivity extends Activity {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_PICTURES);
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File storageDir = cw.getDir("cardImageDirs", Context.MODE_PRIVATE);
         File image = File.createTempFile(
             imageFileName,  // prefix
             ".jpg",         // suffix
@@ -124,20 +120,49 @@ public class MainActivity extends Activity {
 
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        Log.d(TAG,"photo Path:" + mCurrentPhotoPath);
         return image;
+    }
+
+    private String savePhoto(Bitmap bitmapImage){
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            Log.i(TAG, ex.getMessage());
+        }
+        // Continue only if the File was successfully created
+        FileOutputStream fos = null;
+        try {
+
+            fos = new FileOutputStream(photoFile);
+
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return photoFile.getAbsolutePath();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_CANCELED){
+            Log.d(TAG,"result canceled");
+            return;
+        }
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            try {
-                mImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(mCurrentPhotoPath));
-                Bitmap thumbnail = ThumbnailUtils.extractThumbnail(mImageBitmap, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
-                cardView.setImageBitmap(thumbnail);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                Log.d(TAG,"Got a requestCode REQUEST_IMAGE_CAPTURE");
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                cardView.setImageBitmap(imageBitmap);
+                String savePath = savePhoto(imageBitmap);
+                Log.d(TAG,"photo saved to: " + savePath);
         }else if(requestCode == ADD_CONTACT_REQUEST && resultCode == RESULT_OK){
+            Log.d(TAG,"got here");
             String firstName;
             String lastName;
             String cardImgPath;
@@ -158,6 +183,7 @@ public class MainActivity extends Activity {
             Card newCard = new Card(firstName,lastName,thumbnail,cardImgPath);
             CardManager.getInstance(this).addCard(newCard);
         }
+        Log.d(TAG,"Got there");
     }
 
     @Override
