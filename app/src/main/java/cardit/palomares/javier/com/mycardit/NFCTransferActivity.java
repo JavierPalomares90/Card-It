6805ddb140423e9e07aee9d20413531d6231403c
Javier Palomares <Javier Palomares>
2016-04-07 20:15:20 -0500

    Added new NFC Activity

---
+++ app/src/main/java/cardit/palomares/javier/com/mycardit/NFCTransferActivity.java
@@ -0,0 +1,14 @@
+package cardit.palomares.javier.com.mycardit;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NFCTransferActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfctransfer);
+    }
+
+}
