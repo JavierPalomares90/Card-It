9fd46ee28a07a99ca24add168fefb469c70c1465
Javier Palomares <Javier Palomares>
2016-08-02 12:36:27 -0500

    Making simple crop follow material design

--- app/src/main/java/cardit/palomares/javier/com/mycardit/AddContactActivity.java
+++ app/src/main/java/cardit/palomares/javier/com/mycardit/AddContactActivity.java
@@ -313,22 +313,6 @@ public class AddContactActivity extends Activity {
         }
     }
 
-    private boolean hasImageCaptureBug() {
-
-        // list of known devices that have the bug
-        ArrayList<String> devices = new ArrayList<String>();
-        devices.add("android-devphone1/dream_devphone/dream");
-        devices.add("generic/sdk/generic");
-        devices.add("vodafone/vfpioneer/sapphire");
-        devices.add("tmobile/kila/dream");
-        devices.add("verizon/voles/sholes");
-        devices.add("google_ion/google_ion/sapphire");
-
-        return devices.contains(android.os.Build.BRAND + "/" + android.os.Build.PRODUCT + "/"
-                + android.os.Build.DEVICE);
-
-    }
-
     private void cropAndRotateImage(Uri imgUri)
     {
 
--- app/src/main/java/cardit/palomares/javier/com/mycardit/ViewContactActivity.java
+++ app/src/main/java/cardit/palomares/javier/com/mycardit/ViewContactActivity.java
@@ -125,7 +125,7 @@ public class ViewContactActivity extends Activity {
 
             }
             else{
-       
+
             }
             fitToScreen = !fitToScreen;
             return true;
